7cb1475331de71c940c426a745babfb554508ab0
Iqis Gnahz <iqis.gnahz@gmail.com>
2018-12-05 23:03:37 -0500

    ask() delete , ? and whitespace from question

--- R/core.R
+++ R/core.R
@@ -3,7 +3,8 @@
 #' @param question a character string to which the answer is sought
 #' @export
 ask <- function(question){
-        seed <- sum(strtoi(charToRaw(tolower(question)), 16L), as.numeric(Sys.Date()))
+        question <- tolower(gsub("[\\?,]*\\s*", "", question))
+        seed <- sum(strtoi(charToRaw(question), 16L), as.numeric(Sys.Date()))
         set.seed(seed)
         selected_card <- sample(cards, size = 1)
         return(selected_card)

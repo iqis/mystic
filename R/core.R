c932198833fb1f70c053cde0c3ac69956e1e4985
Iqis Gnahz <iqis.gnahz@gmail.com>
2018-12-05 22:42:10 -0500

    added date to random seed

--- R/core.R
+++ R/core.R
@@ -3,7 +3,7 @@
 #' @param question a character string to which the answer is sought
 #' @export
 ask <- function(question){
-        seed <- sum(strtoi(charToRaw(tolower(question)), 16L))
+        seed <- sum(strtoi(charToRaw(tolower(question)), 16L), as.numeric(Sys.Date()))
         set.seed(seed)
         selected_card <- sample(cards, size = 1)
         return(selected_card)
